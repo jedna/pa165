032d7db967a619676dca9d24ec806fa3fc14d622
Jan Hrubes <xhrubes@nymfe36.fi.muni.cz>
2013-10-22 15:43:29 +0200

    NewsApp

---
+++ NewsApp/NewsApp-ejb/src/main/java/ejb/NewMessage.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.ejb.MessageDrivenContext;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author xhrubes
+ */
+@MessageDriven(activationConfig = {
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/NewMessage")
+})
+public class NewMessage implements MessageListener {
+    
+    @Resource
+    private MessageDrivenContext mdc;
+    
+    @PersistenceContext(unitName = "com.ejb_NewsApp-ejb_ejb_1.0-SNAPSHOTPU")
+    private EntityManager em;
+    
+    @Resource
+    private javax.transaction.UserTransaction utx;
+ 
+    
+    
+    public NewMessage() {
+    }
+    
+    @Override
+    public void onMessage(Message message) {
+        ObjectMessage msg = null;
+        try {
+            if (message instanceof ObjectMessage) {
+                msg = (ObjectMessage) message;
+                NewsEntity e = (NewsEntity) msg.getObject();
+                save(e);
+                
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+            mdc.setRollbackOnly();
+        } catch (Throwable te) {
+            te.printStackTrace();
+        }
+    }
+
+    public void save(Object object) {
+        try {
+            utx.begin();
+            em.persist(object);
+            utx.commit();
+        } catch (Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ NewsApp/NewsApp-ejb/src/main/java/ejb/NewsEntityFacade.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author xhrubes
+ */
+@Stateless
+public class NewsEntityFacade extends AbstractFacade<NewsEntity> {
+    @PersistenceContext(unitName = "com.ejb_NewsApp-ejb_ejb_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public NewsEntityFacade() {
+        super(NewsEntity.class);
+    }
+    
+}
